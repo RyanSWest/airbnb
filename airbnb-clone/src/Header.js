b77ab7f690664d2a16fd9ce983bfba7e297aedbf
RyanSWest <west56309@gmail.com>
2021-07-20 18:02:04 -0500

    header and home page

---
+++ airbnb-clone/src/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Header.css';
+
+function Header() {
+    return (
+        <div className = "Header">
+            <h1>Header</h1>
+            <img 
+            className= 'header__icon'
+            src = "https://tse2.mm.bing.net/th?id=OIP.ilW20z977yedyTPY80QsFgHaCw&pid=Api&P=0&w=425&h=159"
+            />
+
+            <div className = 'header__center'>
+                <form>
+                    <input className = 'search'/>
+                    
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Header
