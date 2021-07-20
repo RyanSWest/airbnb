ab1fc5cddc8efb5a5bff778c8e0dca28f33fee90
RyanSWest <west56309@gmail.com>
2021-07-20 18:46:08 -0500

    header stuff

--- airbnb-clone/src/Header.js
+++ airbnb-clone/src/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Header.css';
+import SearchIcon from '@material-ui/icons/Search'
 
 function Header() {
     return (
@@ -13,7 +14,7 @@ function Header() {
             <div className = 'header__center'>
                 <form>
                     <input className = 'search'/>
-                    
+                    <SearchIcon/>
                 </form>
             </div>
         </div>
